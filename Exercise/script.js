fa2d197c4f1935d6277560027d27b4334997eba8
st-baonguyen2 <bao.nguyen2@supremetech.vn>
2022-06-21 16:39:07 +0700

    update file javascript, exercise addToCart

--- Exercise/script.js
+++ Exercise/script.js
@@ -1,53 +1,57 @@
-const addButton = document.querySelectorAll('.product-item')
-const buttonCart = document.querySelectorAll('.btn-cart')
-for(let i=0;i<buttonCart.length;i++){
-  buttonCart[i].addEventListener('click',function(){
-    cartNumbers(products[i])
-    totalCost(products[i]);
-  })
-}
-
-let products=[
-  {
-    name:'T-Shirt Summer Vibes',
-    tag:'image1',
-    price: 89.99,
-    inCart:0
-  },
-  {
-    name:'Loose Knit 3/4 Sleeve',
-    tag:'image-2',
+var products = [{
+    id: 1,
+    name: 'T-Shirt Summer Vibes',
+    imageUrl: 'img/image1.png',
     price: 119.99,
-    inCart:0
+    discount: 0.3,
   },
   {
-    name:'Basic Slim Fit T-Shirt',
-    tag:'image-3',
+    id: 2,
+    name: 'Loose Knit 3/4 Sleeve',
+    imageUrl: 'img/image-2.png',
+    price: 119.99,
+    discount: 0
+  },
+  {
+    id: 3,
+    name: 'Basic Slim Fit T-Shirt',
+    imageUrl: 'img/image-3.png',
     price: 79.99,
-    inCart:0
+    discount: 0
   },
   {
-    name:'Loose Textured T-Shirt',
-    tag:'image-4',
+    id: 4,
+    name: 'Loose Textured T-Shirt',
+    imageUrl: 'img/image-4.png',
     price: 119.99,
-    inCart:0
+    discount: 0
   }
 ]
 
-localStorage.setItem('data', JSON.stringify(products));
+function decimalNumber(num) {
+  return Math.round(num * 100) / 100;
+}
 
+function setData(data, value) {
+  localStorage.setItem(data, JSON.stringify(value));
+}
 
-function renderUI(){
-  let Elementli= document.querySelectorAll('.product-list')
-  let productList = JSON.parse(localStorage.getItem('data'));
-  console.log(Elementli)
-  productList.forEach(item =>{
-    Elementli[1].innerHTML +=`
+function getData(data, value) {
+  value = JSON.parse(localStorage.getItem(data));
+}
+
+function renderUI() {
+  getData('data', products)
+  var Elementli = document.querySelectorAll('.product-list')
+  for (var i = 0; i < Elementli.length; i++) {
+    products.forEach(item => {
+      if (item.discount != 0) {
+        Elementli[i].innerHTML += `
     <li class="product-item col-3">
     <a href="">
      <div class="product-image">
-       <img src="img/${item.tag}.png" alt="T-Shirt Summer Vibes" />
-       <span class="badge badge-promotion">-30%</span>
+       <img src="${item.imageUrl}" alt="T-Shirt Summer Vibes" />
+       <span class="badge badge-promotion">-${item.discount*100}%</span>
      </div>
      <div class="product-infor">
        <div class="infor">
@@ -55,144 +59,126 @@ function renderUI(){
              <h4 class="name-text">${item.name}</h4>
          </div>
          <div class="product-cost">
-           <span class="product-price promotion">$89.99</span>
-           <span class="cost-text">$119.99</span>
+           <span class="product-price promotion">${decimalNumber((item.price-(item.price*item.discount)))}</span>
+           <span class="cost-text">${item.price}</span>
          </div>
        </div>
      </div>
     </a>
     <div class="addToCart">
-     <button class="btn-cart">Add To Cart</button>  
+     <button class="btn-cart" data-id=${item.id}>Add To Cart</button>  
    </div>
-   </li>
-    `
-  })
-}
- renderUI();
-
-function onLoadCartNumber(){
-  let productNumbers= localStorage.getItem('cartNumbers');
-  if(productNumbers){
-    document.querySelector('.cart-count').textContent=productNumbers;
-  }
-}
-
-function cartNumbers(product){
-  let productNumbers= localStorage.getItem('cartNumbers');
-  productNumbers=parseInt(productNumbers)
-  localStorage.setItem('cartNumbers', 1)
-  if(productNumbers){
-    localStorage.setItem('cartNumbers',productNumbers + 1)
-    document.querySelector('.cart-count').textContent=productNumbers+1
-  }
-  else{
-    localStorage.setItem('cartNumbers', 1)
-    document.querySelector('.cart-count').textContent=1
-
-  }
-  
-  setItems(product);
-}
-function setItems(product){
-  let cartItems = localStorage.getItem('productsInCart')
-  cartItems = JSON.parse(cartItems)
-  
-  if(cartItems != null){
-    if(cartItems[product.tag] === undefined){
-      cartItems = {
-        ...cartItems,
-        [product.tag]:product
+   </li>    `
+      } else {
+        Elementli[i].innerHTML += `
+    <li class="product-item col-3">
+    <a href="">
+      <div class="product-image">
+        <img src="${item.imageUrl}" alt="Loose Textured T-Shirt" />
+      </div>
+      <div class="product-infor">
+          <h4 class="name-text">${item.name}</h4>
+        <span class="product-price">$${item.price}</span>
+      </div>
+    </a>
+    <div class="addToCart">
+      <button class="btn-cart" data-id=${item.id}>Add To Cart</button>
+    </div>
+    </li>`
       }
+    })
+  }
+}
+
+function addToCart() {
+  var buttonCart = document.querySelectorAll('.btn-cart');
+  for (var i = 0; i < buttonCart.length; i++) {
+    buttonCart[i].addEventListener('click', function(e) {
+      changeQuantityItemInCart('add', e.target.attributes['data-id'].value);
+      countCart()
+    })
+  }
+}
+
+function changeQuantityItemInCart(active, productId) {
+  var cartData = JSON.parse(localStorage.getItem('cartData'))
+  var quantity = 1;
+  var index = cartData.findIndex(function(cart) {
+    return cart.id == productId
+  });
+  var productSelected = products.find((product) => product.id == productId);
+  if (active == 'add') {
+    if (index < 0) {
+      productSelected['quantity'] = quantity
+      cartData.push(productSelected)
+      setData('cartData', cartData)
+    } else {
+      cartData[index].quantity++
+      setData('cartData', cartData)
     }
-    cartItems[product.tag].inCart += 1
   }
-  else{
-    product.inCart=1;
-    cartItems = {
-      [product.tag]:product
-    }
-  }
-  localStorage.setItem('productsInCart',JSON.stringify(cartItems))
+  console.log('assss', quantity);
+  if (active == 'minus') {}
+
 }
 
-function totalCost(product){
-  let cartCost = localStorage.getItem('totalCost')
-  if(cartCost !=null){
-    cartCost=parseInt(cartCost)
-    localStorage.setItem('totalCost',cartCost+product.price);
+function countCart() {
+  var quantity = 0;
+  var count = 0;
+  var cartData = JSON.parse(localStorage.getItem('cartData'))
+  for (var i = 0; i < cartData.length; i++) {
+    quantity += cartData[i].quantity
   }
-  else{
-    localStorage.setItem('totalCost', product.price)
+  localStorage.setItem('count', quantity)
+  count = localStorage.getItem('count')
+  document.querySelector('.cart-count').innerHTML = count
+}
+window.addEventListener('DOMContentLoaded', function(e) {
+  setData('data', products)
+  renderUI()
+  addToCart()
+  countCart()
+  displayCart()
+});
+
+function cartCost() {
+  var cost = 0;
+  var cartCost = 0
+  var totalCost = JSON.parse(localStorage.getItem('cartData'))
+  for (var i = 0; i < totalCost.length; i++) {
+    cost = decimalNumber(totalCost[i].price* totalCost[i].quantity)
+    cartCost += cost
   }
+  return cartCost
 }
 
-/*----------*/
-function addItem(id) {
-  let cartItems = localStorage.getItem('productsInCart')
-  cartItems = JSON.parse(cartItems)
-  console.log(cartItems[id])
-  cartItems[id].inCart++;
-}
-function removeItem(id) {
-  let cartItems = localStorage.getItem('productsInCart')
-  cartItems = JSON.parse(cartItems)
-  if (cartItems[id] <= 0) return
-  cartItems[id].inCart--;
-}
-
-
-/*----------*/
-
-
-function displayCart(){
-  let cartItems=localStorage.getItem('productsInCart')
-  cartItems=JSON.parse(cartItems)
-  let productContainer=document.querySelector(".cart-main")
-  let cartCost = localStorage.getItem('totalCost')
-  
-  if(cartItems && productContainer){
-      productContainer.innerHTML = '';
-      console.log( Object.values(cartItems))
-      Object.values(cartItems).map(item => {
-          productContainer.innerHTML+=`
-            <div class="product" id='${item.tag}'>
+function displayCart() {
+  var cartItems = JSON.parse(localStorage.getItem('cartData'))
+  var productContainer = document.querySelector(".cart-main")
+  if (cartItems && productContainer) {
+    productContainer.innerHTML = '';
+    Object.values(cartItems).map(item => {
+      productContainer.innerHTML += `
+            <div class="product" id='${item.id}'>
               <div class="product-button">
                 <button class="btn-delete">Delete</button>
               </div>  
                 <span class="main-text">${item.name}</span>
                 <div class="quantity">
                 <i class="fa-solid fa-plus"></i>
-                <span class="incart">${item.inCart}</span>
+                <span class="incart">${item.quantity}</span>
                 <i class="fa-solid fa-minus"></i>
                 </div>
                 <span class="price">$${item.price}</span>
                 
                 <div class="money">
-                ${Math.round(item.inCart*item.price * 100) / 100}<div>
-            </div>
-          `
-      })
-      productContainer.innerHTML +=
-        `<div class="Total">
+                ${decimalNumber(item.price*item.quantity)}<div>
+            </div>  `
+    })
+    productContainer.innerHTML +=
+      `<div class="Total">
             <p class="Total-title">Total: </p>
-            <p class="Total-price">$${cartCost}</p>
+            <p class="Total-price">$${decimalNumber(cartCost())}</p>
         </div>`
-        
   }
 }
-onLoadCartNumber();
-displayCart();
-const productList = document.querySelectorAll('.product')
-console.log(productList)
-productList.forEach(element => {
-  const inscreasetBtn = element.querySelector('.fa-plus')
-  const descreaseBtn = element.querySelector('.fa-minus')
-  console.log(inscreasetBtn,descreaseBtn)
-  const currentId = element.getAttribute('id')
-  inscreasetBtn.addEventListener('click', () => {
-    addItem(currentId)
-  })
-  descreaseBtn.addEventListener('click', () => {
-    removeItem(currentId)
-  })
-});
