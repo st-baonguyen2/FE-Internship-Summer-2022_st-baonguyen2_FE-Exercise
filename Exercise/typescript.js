9e758cf90ca4e341b666a4553f29773df5d76c6c
st-baonguyen2 <bao.nguyen2@supremetech.vn>
2022-06-22 22:36:20 +0700

    delete file empty

--- Exercise/typescript.js
+++
@@ -1,1 +0,0 @@
-console.log('day la file xuat typescript')
--- Exercise/typescript.ts
+++
@@ -1,7 +0,0 @@
-export function decimalNumber(num:any) {
-  return Math.round(num * 100) / 100;
-}
-
-export function setData(data: string, value: any) {
-  localStorage.setItem(data, JSON.stringify(value));
-}
